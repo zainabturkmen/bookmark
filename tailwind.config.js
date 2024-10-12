b335e11db6d5715eb08cb4d1b416706c4ed47a17
zainabturkmen <zainabturkmen7@gmail.com>
2024-10-12 22:33:19 +0430

    Adding Some Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '1020px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
     },
     extend: {
       colors: {
-        softBlue: 'hsl(231, 69%, 60%)',
-        softRed: 'hsl(0, 94%, 66%)',
-        grayishBlue: 'hsl(229, 8%, 60%)',
-        veryDarkBlue: 'hsl(229, 31%, 21%)',
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
       },
       fontFamily: {
-        sans: ['Rubik', 'sans-serif'],
+        sans: ["Rubik", "sans-serif"],
       },
       backgroundImage: () => ({
         dots: "url('../images/bg-dots.svg')",
@@ -23,4 +23,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
