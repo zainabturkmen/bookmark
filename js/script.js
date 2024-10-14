2a6485c2099751104c3543d674f6a39c657fb9a8
zainabturkmen <zainabturkmen7@gmail.com>
2024-10-14 10:59:43 +0430

    Adding Some Changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-
-
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
 
 // Tabs menu event listener
 
-tabs.forEach((tab) => tab.addEventListener("click", onTabClick))
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
 function onTabClick(e) {
-  console.log(e.target);
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    )
+  });
 }
-
 
 // const tabs = document.querySelectorAll('.tab')
 // const panels = document.querySelectorAll('.panel')
