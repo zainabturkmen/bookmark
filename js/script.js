8fd4f21a1ce4465d8575704ae3bbd5b1e80c2806
zainabturkmen <zainabturkmen7@gmail.com>
2024-10-14 11:41:01 +0430

    Adding Some Changes

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function onTabClick(e) {
   // Activate a new tab and panel based on the target
   e.target.classList.add("border-softRed", "border-b-4")
   const classString = e.target.getAttribute("data-target")
+  document.getElementById("panels").getElementsByClassName(classString)[0]
   console.log(classString);
 }
 
