b5a4b29ed29aa0bd06c6f218d7115e57a58de0ec
zainabturkmen <zainabturkmen7@gmail.com>
2024-10-17 09:08:59 +0430

    Adding Some Changes

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
+  
 
   if (menu.classList.contains("flex")) {
     logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
